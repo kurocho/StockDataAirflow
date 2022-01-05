017dde5570e78c1a59407c8510c178cb2d2ebd76
SzymonMajk <Szymek_diks@interia.pl>
2022-01-05 20:37:49 +0100

    stock dag update

--- dags/stock.py
+++ dags/stock.py
@@ -1,56 +1,71 @@
 from airflow.decorators import dag, task
 from datetime import datetime
 from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
+from airflow.exceptions import AirflowException
 
-state = 'wa'
+default_args = {'start_date': datetime(2022, 1, 1)}
+BATCH_SIZE = 10
 
-default_args = {
-    'start_date': datetime(2022, 1, 1)
-}
-
-@dag('stock_data_dag', schedule_interval='@daily', default_args=default_args, catchup=False, tags=['stock'])
+@dag(
+    'stock_data_dag',
+    schedule_interval='@daily',
+    default_args=default_args,
+    catchup=False,
+    tags=['stock'],
+)
 def taskflow():
-
     @task.virtualenv(
         use_dill=True,
         system_site_packages=False,
-        requirements=['beautifulsoup4','requests'],
+        requirements=['beautifulsoup4', 'requests'],
     )
-    def get_tickers(state):
+
+    def get_tickers():
         import requests
         import re
         from bs4 import BeautifulSoup
+
         html_tags_regex = r'<[^<]+?>'
 
         def parse_tickers(ticker_divs):
             return [re.sub(html_tags_regex, '', str(x)) for x in ticker_divs]
-
 
         URL = "https://www.earningswhispers.com/calendar?sb=p&d=1&t=all"
         page = requests.get(URL)
 
         soup = BeautifulSoup(page.content, "html.parser")
         ticker_elements = soup.find_all("div", class_="ticker")
-        return{'tickers': parse_tickers(ticker_elements)}
+
+        return {'tickers': parse_tickers(ticker_elements)}
+    
+    
 
     @task.virtualenv(
         use_dill=True,
-        system_site_packages=False,
-        requirements=['yfinance'],
+        system_site_packages=True,
+        requirements=['yfinance']
     )
-    def get_stock_data(tickers: list):
-        print(tickers)
-
+    def get_stock_data(tickers: list, index, BATCH_SIZE):
         import yfinance as yf
-
         def get_financial_data(ticker):
             stock = yf.Ticker(ticker)
             return stock
 
-        for ticker in tickers['tickers']:
-            get_financial_data(ticker)
+        skipped = True
+        for i in range(0,len(tickers['tickers'])):
+            if i % BATCH_SIZE == index:
+                ticker = tickers['tickers'][i]
+                print("Getting stock data for: " + ticker)
+                get_financial_data(ticker)
+                skipped = False
 
-    tickers_task = get_tickers(state)
-    stock_data_task = get_stock_data(tickers_task)
+        if skipped:
+            print("SKIPPED")
+        
+
+    tickers_task = get_tickers()
+    for i in range(0,BATCH_SIZE):
+        stock_data_task = get_stock_data(tickers_task,i,BATCH_SIZE)
+
 
 dag = taskflow()
